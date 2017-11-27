b8a4dcdfcba2f7749c19705ca367c9e6c3f44dc9
sil <sil@server>
2017-11-27 11:26:13 -0500

    V.3 - creacion y modificacion de la clase HibernateUtil.java

---
+++ HibernateCrudDao/src/Util/HibernateUtil.java
@@ -0,0 +1,60 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Util;
+
+import org.hibernate.Session;
+import org.hibernate.cfg.AnnotationConfiguration;
+import org.hibernate.SessionFactory;
+
+/**
+ * Hibernate Utility class with a convenient method to get Session Factory
+ * object.
+ *
+ * @author sil
+ */
+public class HibernateUtil {
+
+    private static final SessionFactory sessionFactory;
+    private static Session sesion;
+
+    static {
+        try {
+            // Create the SessionFactory from standard (hibernate.cfg.xml) 
+            // config file.
+            sessionFactory = new AnnotationConfiguration().configure().buildSessionFactory();
+        } catch (Throwable ex) {
+            // Log the exception. 
+            System.err.println("Initial SessionFactory creation failed." + ex);
+            throw new ExceptionInInitializerError(ex);
+        }
+    }
+
+    public static SessionFactory getSessionFactory() {
+        return sessionFactory;
+    }
+
+    public static Session getSesion() {
+
+        if (sesion != null && sesion.isOpen()) {
+
+            setSesion(null);
+
+        }
+
+        if (sesion == null) {
+
+            sesion = getSessionFactory().openSession();
+
+        }
+
+        return sesion;
+    }
+
+    public static void setSesion(Session sesion) {
+        HibernateUtil.sesion = sesion;
+    }
+
+}
